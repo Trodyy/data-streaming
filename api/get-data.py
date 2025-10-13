f4d891fcec5ab0b60f49cfb14aa2d171bd1ca1ed
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-10-14 00:03:03 +0330

    FIX : replace api's url.

--- api/get-data.py
+++ api/get-data.py
@@ -1,5 +1,19 @@
 import requests
+print("Hello")
+url = 'https://api.freecryptoapi.com/v1/getCryptoList'
+header = {
+    "accept": "*/*",
+    "Authorization": "Bearer e6oxzv3qp6robho81kia"
+}
 
-url = ''
-response = requests.get(url)
-value = response.json()
+response = requests.get(url , headers=header)
+if response.status_code == 200 :
+    value = response.json()
+    print(value)
+else :
+    print(f"Error: {response.status_code}")
+    print(response.text)
+
+
+#### the follwing code is for api ######
+#https://freecryptoapi.com/
