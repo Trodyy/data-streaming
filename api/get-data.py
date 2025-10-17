1ed390ae440d28a92ef37d7c3c53a98fa5e31e5e
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-10-18 00:08:36 +0330

    FIX : Replace a good api

--- api/get-data.py
+++ api/get-data.py
@@ -1,19 +1,16 @@
 import requests
 import os
 
-url = 'https://api.freecryptoapi.com/v1/getData?symbol=BTC'
-header = {
-    "accept": "*/*",
-    "Authorization": "Bearer e6oxzv3qp6robho81kia"
+url = "https://api.freecryptoapi.com/v1/getData?symbol=BTC"
+
+headers = {
+    'accept': '*/*',
+    'Authorization': 'Bearer e6oxzv3qp6robho81kia'
 }
 
-response = requests.get(url , headers=header)
-if response.status_code == 200 :
-    value = str(response.json())
-    print(value)
-else :
-    print(f"Error: {response.status_code}")
-    print(response.text)
+response = requests.get(url, headers=headers)
+print(response.json())
+
 
 
 #### the follwing code is for api ######
